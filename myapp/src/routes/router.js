c15f24dacb9b5c925794ee60fc3dd379d5ab8f31
bryan <bryanhernandz102@gmail.com>
2022-12-13 16:23:10 -0500

    comentei linha 7 do router.js

--- myapp/src/routes/router.js
+++ myapp/src/routes/router.js
@@ -1,25 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
-import Usuarios from "../componentes/usuarios";
-import Footer from "../componentes/footer";
-import NavBar from "../componentes/navBar";
-import Cursos from "../componentes/cursos";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Usuarios from '../componentes/usuarios';
+import Footer from '../componentes/footer';
+import NavBar from '../componentes/navBar';
+// import Cursos from "../componentes/cursos";
 
-
-const Router =()=>{
-
-return (   
-<BrowserRouter>
-<NavBar />
-    <Routes>
-    <Route exact path="/" element={<Usuarios/>} />
-    </Routes>
-    <Footer />
-    </BrowserRouter>)
-}
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Usuarios />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 export default Router;
