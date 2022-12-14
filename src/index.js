788adf2dcd01feb4eded82ef6be6961a72e8e8e1
bryan <bryanhernandz102@gmail.com>
2022-12-14 08:45:24 -0500

    styling header

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client';
 import '../src/style/style.css';
 import Router from './routes/router';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
